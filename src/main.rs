f9479c1fe8ad5b6ba5680caac51fc97ba649d84f
Alessio Coltellacci <lightplay8@gmail.com>
2018-03-19 18:24:41 +0100

    Remove useless deps and code and setup the env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,20 @@
-extern crate env_logger;
-extern crate itertools;
 #[macro_use]
 extern crate log;
+extern crate env_logger;
 extern crate time;
+extern crate warp10;
 extern crate trust_dns;
 extern crate trust_dns_resolver;
-extern crate warp10;
+
 use std::error::Error;
 use std::str::FromStr;
+
 use trust_dns::client::*;
 use trust_dns::udp::UdpClientConnection;
 use trust_dns::op::Message;
 use trust_dns::rr::{DNSClass, Name, RecordType};
 
-extern crate serde;
-#[macro_use]
-extern crate serde_derive;
-extern crate serde_json;
-
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Debug)]
 pub struct Order {
     pub hostname: String,
     pub write_token: String,
@@ -26,20 +22,15 @@ pub struct Order {
 }
 
 pub fn check(hostname: &str) -> Result<Message, Box<Error>> {
-    // Construct a new Resolver with default configuration options
-    //let resolver = Resolver::new(ResolverConfig::default(), ResolverOpts::default())?;
-    // Lookup the IP addresses associated with a name.
-    //let response = resolver.query(hostname,RecordType::CNAME)?;
-
     let address = "8.8.8.8:53".parse().unwrap();
     let conn = UdpClientConnection::new(address).unwrap();
     let client = SyncClient::new(conn);
     // Specify the name, note the final '.' which specifies it's an FQDN
     let name = Name::from_str(hostname).unwrap();
 
-    // NOTE: see 'Setup a connection' example above
-    // Send the query and get a message response, see RecordType for all supported options
     let response: Message = client.query(&name, DNSClass::ANY, RecordType::ANY).unwrap();
+    trace!("{} response: {:#?}", hostname, response);
+
     Ok(response)
 }
 
@@ -66,11 +57,12 @@ pub fn handle_order(order: &Order) -> Result<(), Box<Error>> {
 }
 
 pub fn main() {
+    env_logger::init();
+
     let order1 = Order{
         hostname: "clever-cloud.fr".to_owned(),
         write_token: "cAM7xGIjBpJcoJ5D4FDpD677YEUXiZZBQEtGt2pJl0Ewjve20HZ_Ows8Yh8Ra5KIuoo5xQZuCulOnfN0FYN6Ck98nwsDvRYBIBQkiklyfoq91KVBbJSlwVOjTv79w0srREOqhgqSSfS7kk0n21GlgeybRRJ6t342fN4f2Y89WoJ".to_owned(),
         warp10_url: "http://hv-par2-021.clvrcld.net:19001/api/v0".to_owned(),
     };
     handle_order(&order1).unwrap();
-    //check(&order1.hostname);
 }
